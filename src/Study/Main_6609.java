c44662636e721397278983b0684c459fb2c21efa
naemhui <knaem.edu@gmail.com>
2024-10-07 20:58:00 +0900

    모기곱셈 자바

---
+++ src/Study/Main_6609.java
@@ -0,0 +1,16 @@
+package Study;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Main_6609 {
+    public static void main(String[] args) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+
+        
+    }
+}
