4106416d14057beb64ee79ab09ab121959230305
naemhui <knaem.edu@gmail.com>
2024-10-11 22:26:09 +0900

    모기 곱셈

--- src/Study/Main_6609.java
+++ src/Study/Main_6609.java
@@ -1,16 +1,32 @@
 package Study;
 
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main_6609 {
     public static void main(String[] args) throws IOException {
-        InputStream in = System.in;
-        InputStreamReader reader = new InputStreamReader(in);
-        BufferedReader br = new BufferedReader(reader);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String tmp;
+        while((tmp=br.readLine())!=null){
+            StringTokenizer st = new StringTokenizer(tmp);
+            int M = Integer.parseInt(st.nextToken()); //모기성충
+            int P = Integer.parseInt(st.nextToken()); //번데기
+            int L = Integer.parseInt(st.nextToken()); // 유충
+            int E = Integer.parseInt(st.nextToken()); // 모기가 낳는 알 수
+            int R = Integer.parseInt(st.nextToken()); // 살아남는 유충
+            int S = Integer.parseInt(st.nextToken()); // 살아남는 번데기
+            int N = Integer.parseInt(st.nextToken()); // N주후 모기
 
-        
+            for(int i =0; i<N;i++){
+                int m = M;
+                M = P/S;
+                P = L/R;
+                L = m*E;
+            }
+
+            System.out.println(M);
+        }
+
     }
 }
