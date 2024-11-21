ae34c55b25ad039f99f510bf7e71cc2067a154d2
naemhui <knaem.edu@gmail.com>
2024-11-21 20:33:02 +0900

    정보보호학부 동아리 소개

---
+++ src/Study/Main_28691.java
@@ -0,0 +1,22 @@
+package Study;
+
+import java.util.Scanner;
+
+public class Main_28691 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        if(a.equals("M")) {
+            System.out.println("MatKor");
+        } else if(a.equals("W")) {
+            System.out.println("WiCys");
+        } else if(a.equals("C")) {
+            System.out.println("CyKor");
+        } else if(a.equals("A")) {
+            System.out.println("AlKor");
+        } else if(a.equals("$")) {
+            System.out.println("$clear");
+        }
+    }
+}
