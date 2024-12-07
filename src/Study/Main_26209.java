3e96ded051d0fc4e5e240c61a4de2c920f9b9c1a
naemhui <knaem.edu@gmail.com>
2024-12-07 17:19:24 +0900

    Intercepting Information

---
+++ src/Study/Main_26209.java
@@ -0,0 +1,23 @@
+package Study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_26209 {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            if (st.nextToken().equals("9")) {
+                System.out.println("F");
+                return;
+            }
+        }
+        System.out.println("S");
+    }
+
+    public static void main(String[] args) throws Exception{
+        new Main_26209().solution();
+    }
+}
