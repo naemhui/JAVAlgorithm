06e9058aff3360e12630ff9c0e269ed9fa0869b7
naemhui <knaem.edu@gmail.com>
2024-12-12 23:06:38 +0900

    홍익대학교

---
+++ src/Study/Main_16394.java
@@ -0,0 +1,13 @@
+package Study;
+
+import java.util.Scanner;
+
+public class Main_16394 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n=scanner.nextInt();
+
+        System.out.println(n-1946);
+    }
+}
