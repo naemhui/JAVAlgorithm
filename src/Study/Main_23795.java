36624ed553e284d546d5e164a79f062180500c12
naemhui <knaem.edu@gmail.com>
2024-12-19 22:58:50 +0900

    사장님 도박은 재미로 하셔야 합니다

---
+++ src/Study/Main_23795.java
@@ -0,0 +1,21 @@
+package Study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_23795 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        while (true) {
+            int cur = Integer.parseInt(br.readLine());
+            if (cur == -1) break;
+            sum += cur;
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main_23795().solution();
+    }
+}
