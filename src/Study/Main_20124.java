d635025e36695458c51b5f1638aeae795359c68a
권남희 <knaem.edu@gmail.com>
2025-02-03 00:10:19 +0900

    소를 찾아라

--- src/Study/Main_20124.java
+++ src/Study/Main_20124.java
@@ -4,39 +4,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main_20124 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Node[] node = new Node[N];
-        for(int i=0;i<N;i++){
+
+        Node[] nodes = new Node[N];
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
             int score = Integer.parseInt(st.nextToken());
-            node[i] = new Node(score,name);
+            nodes[i] = new Node(name, score);
         }
 
-        Arrays.sort(node);
-        System.out.println(node[0].name);
+        // 정렬: 점수 내림차순, 점수가 같으면 이름 오름차순
+        Arrays.sort(nodes, Comparator.comparingInt(Node::score).reversed()
+                .thenComparing(Node::name));
+
+        System.out.println(nodes[0].name);
     }
 
-    static class Node implements Comparable<Node>{
-        int score;
-        String name;
-
-        public Node(int score,String name) {
-            this.score = score;
-            this.name = name;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            if(this.score == o.score)
-                return this.name.compareTo(o.name);
-            else
-                return o.score-this.score;
-        }
-    }
+    record Node(String name, int score) {}
 }
